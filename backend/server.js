1a761860d752b0b5a17945dd38fdeec6d5fc2794
Matt Oestreich <matthewpoestreich@gmail.com>
2019-09-25 10:15:38 -0500

    init

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,6 @@ app.set('ip', '0.0.0.0');
 app.use(HomeController);
 app.use((req, res) => res.status(404).send("Unable to find that")); // 404 middleware
 
-
 app.listen(app.get('port'), app.get('ip'), () => { 
     console.log(`App started on port ${app.get('ip')}:${app.get('port')}`) 
 });
